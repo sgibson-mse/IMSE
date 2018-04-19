fd73debfbfc814ac59125a3e63a36cb5c6d8380d
sgibson-mse <sam.gibson@durham.ac.uk>
2018-04-19 14:36:59 +1000

    Image demodulated to extract phase offset for each FLC state and polarizer angle. Does phase jump correction but isn't working well at the edges. Some issues with 1D FFT filtering, attempt to do 2D filtering and 2D phase jump correction.

--- Analysis/demodulate_image.py
+++ Analysis/demodulate_image.py
@@ -4,29 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-
 #Internal imports
 from Analysis.read_binary import load_binary
 from Analysis.peak_find import indexes
 
-def prepare_image(filename):
+def prepare_image(filename, frames):
 
     images = load_binary(filename, FLC=True)
-
-    image = images[:, :, 1].astype(np.float64)
+    image = images[:, :, frames].astype(np.float64)
     image = image / np.max(image)
 
     return image
 
 def take_slice(image, pixel):
 
-    image_slice = image[int(pixel), :]  # slice image at the center pixel
+    image_slice = image[int(pixel),:]# slice image at the center pixel
 
     return image_slice
 
 #make the window
 
-def make_window(image_slice, n_pixels):
+def make_window(image_slice, ny):
 
     fft_column = np.fft.fftshift(np.fft.fft(image_slice))
 
@@ -35,39 +33,67 @@ def make_window(image_slice, n_pixels):
 
     fringe_peaks = indexes(abs(image_slice), thres=0.7, min_dist=10)
 
-    fringe_width = int((fringe_peaks[1] - fringe_peaks[0]) / 2)
+    try:
+        fringe_width = int((fringe_peaks[1] - fringe_peaks[0]) / 2)
+    except IndexError:
+        fringe_width = 4
 
-    window = np.zeros((n_pixels))
+    window = np.zeros((ny))
 
-    #Design filter around the positive and negative carrier frequency
+    #Design filter around the positive or negative carrier frequency
 
-    window[peaks[0] - fringe_width: peaks[0] + fringe_width + 1] = signal.hann(fringe_width*2+1)
+    #window[peaks[0] - fringe_width: peaks[0] + fringe_width + 1] = signal.hann(fringe_width*2+1)
     window[peaks[-1] - fringe_width: peaks[-1] + fringe_width + 1 ] = signal.hann(fringe_width*2+1)
 
     return window, fringe_width, peaks, fft_column
 
 def filter_image(window, fft_column):
 
-    # just take half the window, and the positive frequency for filtering.
-    window = window[int(len(window) / 2):]
-
-    half_fftcolumn = fft_column[int(len(fft_column) / 2):]
-
-    filtered_column = window * half_fftcolumn
+    filtered_column = window * fft_column
 
     carrier_frequency = np.where(np.max(abs(filtered_column)))
 
     ifft_column = np.fft.ifft(filtered_column)
 
-    intensity = 2 * abs(ifft_column)
-    new_pixels = np.arange(0, ny, 2)
+    intensity = 2 * abs(ifft_column) #amplitude of the carrier frequency
 
+    #phase = np.arctan2(ifft_column.imag, ifft_column.real)
     phase = np.angle(ifft_column)
 
     return intensity, phase
 
+# from Pycis - import the module when I can clone the git.
 
+def unwrap(phase_array):
 
+    y_pix, x_pix = np.shape(phase_array)
+    phase_contour = -np.unwrap(phase_array[int(np.round(y_pix / 2)), :])
+
+    # sequentially unwrap image columns:
+    phase_uw_col = np.zeros_like(phase_array)
+
+    for i in range(0, x_pix):
+        phase_uw_col[:, i] = np.unwrap(phase_array[:, i])
+
+    phase_contour = phase_contour + phase_uw_col[int(np.round(y_pix / 2)), :]
+    phase_0 = np.tile(phase_contour, [y_pix, 1])
+    phase_uw = phase_uw_col - phase_0
+
+    # wrap image centre into [-pi, +pi] (assumed projection of optical axis onto detector)
+    y_centre_idx = np.round((np.size(phase_uw, 0) - 1) / 2).astype(np.int)
+    x_centre_idx = np.round((np.size(phase_uw, 1) - 1) / 2).astype(np.int)
+    phase_uw_centre = phase_uw[y_centre_idx, x_centre_idx]
+
+    if phase_uw_centre > 0:
+        while abs(phase_uw_centre) > np.pi:
+            phase_uw -= 2*np.pi
+            phase_uw_centre = phase_uw[y_centre_idx, x_centre_idx]
+    else:
+        while abs(phase_uw_centre) > np.pi:
+            phase_uw += 2*np.pi
+            phase_uw_centre = phase_uw[y_centre_idx, x_centre_idx]
+
+    return phase_uw
 
 #Extract the images from the binary file
 filename = str(os.getcwd()) + '/sam_8.dat'
@@ -75,11 +101,54 @@ filename = str(os.getcwd()) + '/sam_8.dat'
 nx = 1280
 ny = 1080
 n_frames = 38
+n_states = 2
+frames = np.arange(0,38,1)
 pixels = np.arange(0,ny,1)
-image = prepare_image(filename)
 
+Intensity = np.zeros((nx, ny, n_frames))
+Phase = np.zeros((nx, ny, n_frames))
+filtered_image = np.zeros((nx, ny, n_frames))
+Theta = np.zeros((nx,ny,n_frames))
 
+for n in frames:
+    image = prepare_image(filename, frames[n])
 
+    for i in range(len(pixels)):
+        image_slice = take_slice(image, pixels[i])
+        window, fringe_width, peaks, fft_column = make_window(image_slice, ny)
+        intensity_slice, phase_slice = filter_image(window, fft_column)
+        Intensity[i,:,n] = intensity_slice
+        Phase[i,:,n] = phase_slice
+
+phase_unwrapped = np.zeros((nx,ny,n_frames))
+phase_differences = []
+
+for n in range(n_frames):
+    phase_unwrapped[:,:,n] = unwrap(Phase[:,:,n])
+
+for n in range(n_frames-1):
+    phase_difference = phase_unwrapped[:,:,n+1] - phase_unwrapped[:,:,n]
+    phase_differences.append(phase_difference)
+
+phase_diff = np.asarray(phase_differences)
+phase_diff = phase_diff[::2,:,:]
+
+center_y = np.round((ny - 1) / 2).astype(np.int)
+center_x = np.round((nx - 1) / 2).astype(np.int)
+
+central_offsets = phase_diff[:,center_y, center_x]*(180./np.pi)
+
+rotary_stage_angles = np.arange(0,190,10)
+
+plt.figure()
+plt.plot(rotary_stage_angles, central_offsets/4.)
+plt.show()
+
+plt.figure()
+plt.imshow(phase_diff[0,:,:]*(180/np.pi))
+plt.colorbar()
+plt.clim(-12,+12)
+plt.show()
 
 def plot_image(image):
 
@@ -133,7 +202,8 @@ def plot_peaks(image_slice, fringe_peaks):
 
     return
 
-def plot_output():
+def plot_output(half_fftcolumn, window, phase, image_slice, new_pixels, intensity):
+
     plt.figure()
     plt.plot(abs(half_fftcolumn)/np.max(abs(half_fftcolumn)), label='data')
     plt.plot(window, label='window')
@@ -151,4 +221,45 @@ def plot_output():
     plt.show()
 
     return
+
+
+
+#find the phase at the center of the column
+#find difference between first pixel and the center column
+#find it as an integer of pi [-pi, 0, pi]
+#then add or subtract this from the value
+#repeat for each row
+
+# #Unwrap the phase
+#
+# phase_array = Phase[:,:,0]
+#
+# unwrapped_phase_array = np.zeros((nx,ny))
+#
+# for row in range(nx):
+#
+#     phase_slice = phase_array[row,:]
+#
+#     center_x = int((len(phase_array[row,:])-1)/2)
+#
+#     center_phase = phase_slice[center_x]
+#
+#     #center_y = int((len(phase_array[:,0])-1)/2)
+#
+#     for i in range(len(phase_slice)):
+#         #find phase difference as integer of pi
+#         phase_diff = int((phase_slice[i] - center_phase) / np.pi)
+#
+#         if phase_diff == 0:
+#             phase_slice[i] = phase_slice[i]
+#         if phase_diff == 1:
+#             phase_slice[i] += np.pi
+#         if phase_diff == -1:
+#             phase_slice[i] -= np.pi
+#
+#         unwrapped_phase_array[row,i] = phase_slice[i]
+#
+# plt.figure()
+# plt.imshow(unwrapped_phase_array)
+# plt.show()
 
