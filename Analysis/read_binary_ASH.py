04d701610b283fb67ece7ff47175d247315adc8a
sgibson-mse <sam.gibson@durham.ac.uk>
2018-05-31 10:13:12 +1000

    Read binary files for single switch (ASH) or double (FLC) - they just have different number of bytes in the header so need slightly different processing.

--- Analysis/read_binary_ASH.py
+++ Analysis/read_binary_ASH.py
@@ -10,8 +10,6 @@ def load_binary(filename, FLC):
     :return: Array containing two images for each rotation.
     """
 
-    n_headers = 3 #nx, ny, and n_frames
-
     with open(filename,'rb') as f:
 
         header = np.fromfile(f, dtype=np.int16, count=2)
@@ -19,15 +17,17 @@ def load_binary(filename, FLC):
         nx = header[0]
         ny = header[1]
 
-        header_32bit = np.fromfile(f, dtype=np.int32, count=2)
+        header_32bit = np.fromfile(f, dtype=np.int32, count=1)
 
         n_frames = header_32bit[0]
-        navg = header_32bit[1]
 
-        header_floats = np.fromfile(f, dtype=np.float32, count=2)
+        header_floating = np.fromfile(f, dtype=np.float32, count=1)
 
-        step = header_floats[0]
-        theta = header_floats[1]
+        step = header_floating[0]
+
+        header_32 = np.fromfile(f, dtype=np.int32, count=1)
+
+        theta0 = header_32[0]
 
         header_32bit_final = np.fromfile(f, dtype=np.int32, count=1)
 
@@ -39,14 +39,38 @@ def load_binary(filename, FLC):
         else:
             pass
 
-        data = np.fromfile(f, dtype=np.int32)
-
-        print(len(data))
+        data = np.fromfile(f, dtype=np.uint16)
 
         #Order must be fortran-like to preserve the memory layout
 
         images = data.reshape(nx, ny, n_frames, order='F')
 
-    return images
+    return images, step, theta0
 
-load_binary(filename='/home/sam/Desktop/Projects/IMSE-MSE/Analysis/sam_11.dat', FLC=False)
+images, step, theta0 = load_binary(filename='/home/sam/Desktop/Projects/IMSE-MSE/Analysis/sam_11.dat', FLC=False)
+
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+#calculate the offset between the polariser angle vs what we put in on the
+
+n_frames = 37
+theta = np.linspace(theta0,360.,n_frames)*(np.pi/180.)
+
+def fit(theta,a):
+    return 0.25 + 0.25*np.cos(2*(-a+(3*np.pi/4.)-theta))
+
+image_slice =  images[1080,1180,:]
+
+guess = [1]
+popt, pcov = curve_fit(fit, theta, image_slice, p0=guess)
+
+y_fit = image_slice*fit(theta,popt)
+
+plt.figure()
+plt.plot(theta, images[1080,1280,:]/np.max(images[1028,1280,:]))
+plt.plot(theta, y_fit/np.max(y_fit), '--', label='fit')
+plt.legend()
+plt.ylabel('I')
+plt.xlabel('Polariser angle (degrees)')
+plt.show()
