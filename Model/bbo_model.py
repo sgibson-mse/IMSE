19c28fb50997033b4f077810323d35c8ac4f04af
sgibson-mse <sam.gibson@durham.ac.uk>
2018-06-04 14:12:02 +1000

    Changed to crystal class with name as an input. Calculates the refractive indices for both alpha-bbo and lithium niobate crystals

--- Model/bbo_model.py
+++ Model/bbo_model.py
@@ -4,22 +4,33 @@ import matplotlib.pyplot as plt
 from Model.Physics_Constants import Constants
 constants = Constants()
 
-class AlphaBBO(object):
+class Crystal(object):
 
-    def __init__(self, wavelength, thickness, cut_angle):
+    def __init__(self, wavelength, thickness, cut_angle, name):
 
-        self.wavelength = wavelength * 10**-4 #convert to micrometers
+        self.wavelength = wavelength * 10**-3 #convert to micrometers
         self.cut_angle = cut_angle * (np.pi/180.)
-        self.alpha = 4.7 * (180./np.pi) #non axial ray incidence angle
+        #self.alpha = alpha * (180./np.pi) #non axial ray incidence angle
         self.L = thickness #um crystal thickness
-        self.focal_length = 85*10**3 #micrometers
+        self.focal_length = 85000 #micrometers
         self.angular_frequency = 2 * np.pi * (constants.c / self.wavelength)
-        self.sc_ne = [2.3753, 0.012240, -0.016670, -0.01516]
-        self.sc_no = [2.7359, 0.018780, -0.018220, -0.01354]
         self.beta = np.arange(0,2*np.pi)
+        self.name = name
 
-        self.ne = self.refractive_index(sellmeier_coefficients=self.sc_ne)
-        self.no = self.refractive_index(sellmeier_coefficients=self.sc_no)
+        if self.name == 'lithium_niobate':
+            self.sc_ne = [2.9804, 0.02047, 0.5981, 0.0666, 8.9543, 416.08]
+            self.sc_no = [2.6734, 0.01764, 1.2290, 0.05914, 12.614, 474.60]
+
+            self.ne = self.refractive_index(sellmeier_coefficients=self.sc_ne, name=self.name)
+            self.no = self.refractive_index(sellmeier_coefficients=self.sc_no, name=self.name)
+
+        if self.name == 'alpha_bbo':
+
+            self.sc_ne = [2.3753, 0.012240, -0.016670, -0.01516]
+            self.sc_no = [2.7359, 0.018780, -0.018220, -0.01354]
+
+            self.ne = self.refractive_index(sellmeier_coefficients=self.sc_ne, name=self.name)
+            self.no = self.refractive_index(sellmeier_coefficients=self.sc_no, name=self.name)
 
         self.birefringence = self.ne - self.no
 
@@ -29,10 +40,19 @@ class AlphaBBO(object):
 
         self.phi_shear = self.phase_shear()
 
-    def refractive_index(self, sellmeier_coefficients):
+        self.phi_hyperbolic = self.phase_hyperbolic()
 
-        return np.sqrt(sellmeier_coefficients[0] + (sellmeier_coefficients[1] / ((self.wavelength ** 2) +
-                   sellmeier_coefficients[2])) + (sellmeier_coefficients[3] * (self.wavelength ** 2)))
+    def refractive_index(self, sellmeier_coefficients, name):
+
+        if name == 'lithium_niobate':
+            return np.sqrt(1 + (sellmeier_coefficients[0]*self.wavelength**2)/(self.wavelength**2 - sellmeier_coefficients[1]) + (sellmeier_coefficients[2]*self.wavelength**2)/(self.wavelength**2 - sellmeier_coefficients[3]) + (sellmeier_coefficients[4]*self.wavelength**2) / (self.wavelength**2 - sellmeier_coefficients[5]))
+
+        if name == 'alpha_bbo':
+            return np.sqrt(sellmeier_coefficients[0] + (sellmeier_coefficients[1] / ((self.wavelength ** 2) +
+                                                                              sellmeier_coefficients[2])) + (
+                                sellmeier_coefficients[3] * (self.wavelength ** 2)))
+        else:
+            print('Crystal not yet implemented!')
 
     def frequency_dispersion(self):
 
@@ -52,10 +72,9 @@ class AlphaBBO(object):
         return self.phi_0
 
     def phase_shear(self):
-        self.phi_shear = (2 * np.pi * self.L * self.birefringence * np.sin(2*self.cut_angle))/(2*self.wavelength* ((self.ne + self.no)/2) * self.focal_length)
+        self.phi_shear = (2 * np.pi * self.L*10**-6 * self.birefringence * np.sin(2*self.cut_angle))/(2*self.wavelength*10**-6 * ((self.ne + self.no)/2) * self.focal_length*10**-6)
         return self.phi_shear
 
-    def find_pixel_positions(self):
-        self.x = -1 * self.alpha * self.focal_length * np.cos(self.beta)
-
-
+    def phase_hyperbolic(self):
+        self.phi_hyperbolic = -2*np.pi * self.L*10**-6 * self.birefringence / ( 4 * ((self.ne + self.no)/2)**2 * self.wavelength*10**-6 * (self.focal_length*10**-6)**2 )
+        return self.phi_hyperbolic
