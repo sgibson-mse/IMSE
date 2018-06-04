4e4a0c9d82e1622b1c417d94d6e0223d9db6a41a
sgibson-mse <sam.gibson@durham.ac.uk>
2018-06-04 14:12:32 +1000

    Added useful plots and standard text sizes

--- Model/calculate_contrast.py
+++ Model/calculate_contrast.py
@@ -12,6 +12,18 @@ from Model.read_halfenergy import ResolutionHalf, SpectralDataHalf
 from Model.read_thirdenergy import ResolutionThird, SpectralDataThird
 from Model.Physics_Constants import Constants
 
+SMALL_SIZE = 12
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 18
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 def get_stokes_components(spectral_data, resolution):
 
     '''
@@ -33,7 +45,18 @@ def get_stokes_components(spectral_data, resolution):
 
     for i in range(len(stokes_full)):
         linearly_polarised[i,:] = stokes_full[i,1,:] + stokes_full[i,2,:]
-        linearly_polarised[i,:] = linearly_polarised[i,:]/np.max(linearly_polarised[i,:])
+        #linearly_polarised[i,:] = linearly_polarised[i,:]/np.max(linearly_polarised[i,:])
+
+    plt.figure()
+    plt.title('Stokes components at R = {}m'.format("%.2f" % major_radius[1]))
+    plt.plot(wavelength_vector/10, sigma_stokes_full[1,0,:]/10**7, color='black', label='S0 Total Intensity')
+    plt.plot(wavelength_vector/10, linearly_polarised[1,:]/10**7, '--', color='red', label='Linearly polarised')
+    plt.plot(wavelength_vector/10, sigma_stokes_full[1,3,:]/10**7, '-.', color='blue', label='Circularly Polarised')
+    plt.legend(prop={'size': 10})
+    plt.xlim(659.2,659.8)
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Intensity [10$^{7}$photons/s]')
+    plt.show()
 
     return major_radius, stokes_full, wavelength_vector, linearly_polarised
 
@@ -68,9 +91,8 @@ def define_crystals(wavelength_vector, delay_thickness, delay_cut_angle, displac
     :return: Separate instances of the AlphaBBO class - parameters for the displacer and delay plates.
     """
 
-    delay = AlphaBBO(wavelength_vector, thickness=delay_thickness, cut_angle=delay_cut_angle)
-    displacer = AlphaBBO(wavelength_vector, thickness=displacer_thickness, cut_angle=displacer_cut_angle)
-    print(displacer.ne, displacer.no)
+    delay = AlphaBBO(wavelength_vector, thickness=delay_thickness, cut_angle=delay_cut_angle, alpha=7)
+    displacer = AlphaBBO(wavelength_vector, thickness=displacer_thickness, cut_angle=displacer_cut_angle, alpha=7)
 
     return delay, displacer
 
@@ -102,7 +124,17 @@ def add_delay(I_total, I_stokes_full, delay, displacer):
 
     return contrast, total_delay
 
-def find_optimal_delay_thickness(wavelength_vector, stokes_full):
+def angle_of_incidence():
+
+    effective_focal_length = 85 #mm
+    pixel_xpositions = np.linspace(-10.24,10.24,21)
+    pixel_ypositions = np.zeros((len(pixel_xpositions)))
+    alpha = np.arctan2(pixel_xpositions, effective_focal_length)
+    beta = np.arctan2(pixel_ypositions, pixel_xpositions)
+
+    return alpha, beta, pixel_xpositions, effective_focal_length
+
+def find_optimal_delay_thickness(wavelength_vector, stokes_full, channel_full):
 
     """
     Find out what the optimal delay plate thickness should be. Increased thickness means more delay, but increases weight of the higher order terms in the delay equation.
@@ -118,7 +150,7 @@ def find_optimal_delay_thickness(wavelength_vector, stokes_full):
 
     shape = stokes_full.shape #n_pixels, n_stokes, n_intensities
 
-    contrasts = np.zeros((len(pixels),len(delay_thickness)))
+    contrasts = np.zeros((len(pixels),len(displacer_cut_angle)))
     total_polarised = np.zeros((len(pixels),shape[2]))
     phi_total = []
 
@@ -128,24 +160,46 @@ def find_optimal_delay_thickness(wavelength_vector, stokes_full):
         I_total, I_polarised, SN = get_intensities(I_stokes_full)
         total_polarised[i,:] = I_total
 
-        for l in range(len(delay_thickness)):
-
-            delay, displacer = define_crystals(wavelength_vector, delay_thickness[l], delay_cut_angle, displacer_thickness, displacer_cut_angle)
+        for l in range(len(displacer_cut_angle)):
+            delay, displacer = define_crystals(wavelength_vector, delay_thickness, delay_cut_angle, displacer_thickness, displacer_cut_angle[l])
             contrast, total_delay = add_delay(I_total, I_stokes_full, delay, displacer)
             contrasts[i,l] = contrast
             phi_total.append(total_delay)
 
     return contrasts, total_polarised, phi_total
 
-def angle_of_incidence():
+def calc_fringe_frequency(wavelength_vector_1, displacer_cut_angle):
 
-    effective_focal_length = 85 #mm
-    pixel_xpositions = np.linspace(-10.24,10.24,21)
-    pixel_ypositions = np.zeros((len(pixel_xpositions)))
-    alpha = np.arctan2(pixel_xpositions, effective_focal_length)
-    beta = np.arctan2(pixel_ypositions, pixel_xpositions)
+    wavelength = 660*10**-9
+    pixel_size = 20*10**-6
+    focal_lengths = np.array([50*10**-3, 85*10**-3])
+    pixels_per_fringe = np.zeros((len(displacer_cut_angle), len(focal_lengths)))
 
-    return alpha, beta, pixel_xpositions, effective_focal_length
+
+    for j in range(len(focal_lengths)):
+        for i in range(len(displacer_cut_angle)):
+            waves_per_pixel = (2*np.pi*(3*10**-3)*0.117*np.sin(2*displacer_cut_angle[i]*(np.pi/180.)))/(1.61*(focal_lengths[j])*(660*10**-9))
+            waves_per_m = waves_per_pixel*pixel_size/(2*np.pi)
+            pixels_per_fringe[i,j] = 1/waves_per_m
+
+    'plot of pixels per fringe for a given focal length lens, varying displacer cut angle'
+
+    plt.figure()
+    plt.plot(displacer_cut_angle, pixels_per_fringe[:,0], label='$f$ = 50mm')
+    plt.plot(displacer_cut_angle, pixels_per_fringe[:,1], label='$f$ = 85mm')
+    plt.legend()
+    plt.ylim(5,20)
+    plt.xlabel('Displacer cut angle [degrees]')
+    plt.ylabel('Number of pixels per fringe')
+    plt.show()
+
+    plt.figure()
+    plt.plot(displacer_cut_angle, 1/np.array(pixels_per_fringe))
+    plt.xlabel('Displacer cut angle [degrees]')
+    plt.ylabel('fringes per pixel')
+    plt.show()
+
+    return pixels_per_fringe
 
 def find_fwhm(x,y):
 
@@ -187,15 +241,42 @@ def design_filter_2(alpha, lambda_0, n_material, tilt_angle, wavelength_vector_1
 
     return interp_transmittance, lambda_nm
 
-delay_thickness = [15000]
+def plot_cut_angle(contrasts, displacer_cut_angle, major_radius):
+
+    pp,ll = np.meshgrid(displacer_cut_angle,major_radius)
+
+    levels=np.arange(0,0.43,0.05)
+
+    plt.figure()
+    plt.title('Displacer plate = 3mm, Delay = 15mm')
+    plt.pcolormesh(ll,pp,contrasts,shading='gouraud')
+    cbar = plt.colorbar()
+    cbar.set_label('Contrast', rotation=90)
+    plt.xlabel('Major Radius [m]')
+    plt.ylabel('Cut angle $\Theta$ [degrees]')
+    plt.show()
+
+    plt.figure()
+    plt.title('Displacer plate = 3mm, Delay plate = 15mm')
+    CS =plt.contour(ll,pp,contrasts, levels=levels)
+    plt.clabel(CS, inline=1, fontsize=10)
+    cbar2 = plt.colorbar()
+    plt.xlabel('Major Radius [m]')
+    plt.ylabel('Cut angle $\Theta$ [degrees]')
+    cbar2.set_label('Contrast', rotation=90)
+    plt.show()
+
+    return
+
+delay_thickness = 15000.
 delay_cut_angle = 0.
 
-displacer_cut_angle= 45.
+displacer_cut_angle= np.arange(0.,90.,1)
 displacer_thickness = 3000
 
 # define a central wavelength for the filter - the middle of our wavelength array for now
 lambda_0 = 660.5
-scale_fwhm = 1.7
+scale_fwhm = 1.9
 
 n_material = 2  # refractive index of the material
 
@@ -203,86 +284,84 @@ tilt_angle = -1 * (np.pi / 180.)
 
 constants = Constants()
 
-#FULL ENERGY COMPONENT
+# FULL ENERGY COMPONENT
 idl.execute("restore, '/home/sam/Desktop/msesim/runs/mast_imse_photron/output/data/density2e19m3_MAST_photron.dat', /VERBOSE")
 
 channel_full = ChannelFull()
 resolution_full = ResolutionFull()
 spectral_full = SpectralDataFull()
 
-major_radius_1, stokes_1, wavelength_vector_1, linearly_polarised_1 = get_stokes_components(spectral_full, resolution_full)
-
-contrasts, total_polarised, phi_total = find_optimal_delay_thickness(wavelength_vector_1, stokes_1)
+major_radius_1, stokes_1, wavelength_vector_1, linearly_polarised_1 = get_stokes_components(spectral_full,
+                                                                                            resolution_full)
+contrasts, total_polarised, phi_total = find_optimal_delay_thickness(wavelength_vector_1, stokes_1, channel_full)
 alpha, beta, pixel_xpositions, effective_focal_length = angle_of_incidence()
 
-interp_transmittance, lambda_nm = design_filter_2(alpha, lambda_0, n_material, tilt_angle, wavelength_vector_1, scale_fwhm)
-
-I_full = stokes_1[:,1,:] / np.max(abs(stokes_1[:, 1, :]))
-
-ll, rr = np.meshgrid(wavelength_vector_1 / 10, major_radius_1)
-
-plt.figure(1)
-plt.title('Filter CWL $\lambda$={} nm, FWHM = {} nm EFL = {} mm - Full Energy'.format(lambda_0, scale_fwhm, effective_focal_length))
-cont = plt.contour(ll, rr, interp_transmittance, cmap='gray')
-plt.clabel(cont, inline=1, fontsize=10)
-plt.pcolormesh(ll, rr, I_full)
-cs = plt.colorbar()
-cs.set_label('Intensity [Arb Units]', rotation=90.)
-plt.ylabel('Major radius [m]')
-plt.xlabel('Wavelength of spectrum [nm]')
-plt.show()
-
-# #HALF ENERGY COMPONENT
-
-idl.execute("restore, '/home/sam/Desktop/msesim/runs/mast_imse_photron_half/output/data/density2e19m3_MAST_photron.dat' , /VERBOSE")
-
-resolution_half = ResolutionHalf()
-spectral_half = SpectralDataHalf()
-
-major_radius_2, stokes_2, wavelength_vector_2, linearly_polarised_2 = get_stokes_components(spectral_half, resolution_half)
-lambda_half, r_half = np.meshgrid(wavelength_vector_2/10, major_radius_2)
-I_half = stokes_2[:, 1, :] / np.max(abs(stokes_2[:, 1, :]))
-
-plt.figure(2)
-plt.title('Filter CWL $\lambda$={} nm, FWHM = {} nm, EFL = {} mm - Half Energy'.format(lambda_0, scale_fwhm, effective_focal_length))
-cont2 = plt.contour(ll, rr, interp_transmittance, cmap='gray')
-plt.clabel(cont2, inline=1, fontsize=10)
-plt.pcolormesh(lambda_half, r_half, I_half)
-cs = plt.colorbar()
-cs.set_label('Intensity [Arb Units]', rotation=90.)
-plt.ylabel('Major radius [m]')
-plt.xlabel('Wavelength of spectrum [nm]')
-plt.show()
-
-
-
-
-
-
-
-
-
-
+# plot_cut_angle(contrasts, displacer_cut_angle, major_radius_1)
 #
-# #THIRD ENERGY COMPONENT
-# idl.execute("restore, '/home/sam/Desktop/msesim/runs/mast_imse_pcocam_thirdenergy/output/data/density2e19m3_MAST_third.dat' , /VERBOSE")
-#
-# resolution_third = ResolutionThird()
-# spectral_third = SpectralDataThird()
-# major_radius_3, stokes_3, wavelength_vector_3, linearly_polarised_3 = get_stokes_components(spectral_third, resolution_third)
-# lambda_third, r_third = np.meshgrid(wavelength_vector_3/10, major_radius_3)
-# I_third = stokes_3[:, 1, :] / np.max(abs(stokes_3[:, 1, :]))
-#
-# plt.figure(3)
-# plt.title('Filter CWL $\lambda$={} nm, FWHM = {} nm, EFL = {} mm - Third Energy'.format(lambda_0, fwhm, effective_focal_length))
-# cont3 = plt.contour(lambda_full, r_full, interp_transmittance, cmap='gray')
-# plt.clabel(cont3, inline=1, fontsize=10)
-# plt.pcolormesh(lambda_third, r_third, I_third)
-# cs = plt.colorbar()
-# cs.set_label('Intensity [Arb Units]', rotation=90.)
-# plt.ylabel('Major radius [m]')
-# plt.xlabel('Wavelength of spectrum [nm]')
-# plt.show()
+# calc_fringe_frequency(wavelength_vector_1,displacer_cut_angle)
+
+
+def plot_full_energy_component(channel_full, resolution_full, spectral_full):
+
+    major_radius_1, stokes_1, wavelength_vector_1, linearly_polarised_1 = get_stokes_components(spectral_full, resolution_full)
+
+    print('Wavelengths at 63kV -', wavelength_vector_1/10)
+
+    contrasts, total_polarised, phi_total = find_optimal_delay_thickness(wavelength_vector_1, stokes_1, channel_full)
+    alpha, beta, pixel_xpositions, effective_focal_length = angle_of_incidence()
+
+    interp_transmittance_full, lambda_nm = design_filter_2(alpha, lambda_0, n_material, tilt_angle, wavelength_vector_1, scale_fwhm)
+
+    I_full = stokes_1[:,1,:] / np.max(abs(stokes_1[:, 1, :]))
+
+    ll, rr = np.meshgrid(wavelength_vector_1 / 10, major_radius_1)
+
+    plt.figure(1)
+    plt.title('Filter CWL $\lambda$={} nm, FWHM = {} nm EFL = {} mm - Full Energy'.format(lambda_0, scale_fwhm, effective_focal_length))
+    cont = plt.contour(ll, rr, interp_transmittance_full, cmap='gray')
+    plt.clabel(cont, inline=1, fontsize=10)
+    plt.pcolormesh(ll, rr, I_full)
+    cs = plt.colorbar()
+    cs.set_label('Intensity [Arb Units]', rotation=90.)
+    plt.ylabel('Major radius [m]')
+    plt.xlabel('Wavelength of spectrum [nm]')
+    plt.show()
+
+    return ll, rr, interp_transmittance_full, lambda_nm
+
+def plot_half_energy_component():
+    # #HALF ENERGY COMPONENT
+
+    idl.execute(
+        "restore, '/home/sam/Desktop/msesim/runs/mast_imse_photron_half/output/data/density2e19m3_MAST_photron.dat' , /VERBOSE")
+
+    resolution_half = ResolutionHalf()
+    spectral_half = SpectralDataHalf()
+
+    major_radius_2, stokes_2, wavelength_vector_2, linearly_polarised_2 = get_stokes_components(spectral_half,
+                                                                                                resolution_half)
+    lambda_half, r_half = np.meshgrid(wavelength_vector_2 / 10, major_radius_2)
+    I_half = stokes_2[:, 1, :] / np.max(abs(stokes_2[:, 1, :]))
+    interp_transmittance_half, lambda_nm = design_filter_2(alpha, lambda_0, n_material, tilt_angle, wavelength_vector_1, scale_fwhm)
+
+    ll, rr = np.meshgrid(wavelength_vector_1 / 10, major_radius_1)
+
+    plt.figure(2)
+    plt.title('Filter CWL $\lambda$={} nm, FWHM = {} nm, EFL = {} mm - Full Energy'.format(lambda_0, scale_fwhm,
+                                                                                           effective_focal_length))
+    cont2 = plt.contour(ll, rr, interp_transmittance_half, cmap='gray')
+    plt.clabel(cont2, inline=1, fontsize=10)
+    plt.pcolormesh(lambda_half, r_half, I_half)
+    cs = plt.colorbar()
+    cs.set_label('Intensity [Arb Units]', rotation=90.)
+    plt.ylabel('Major radius [m]')
+    plt.xlabel('Wavelength of spectrum [nm]')
+    plt.show()
+
+    return interp_transmittance_half, lambda_half
+
+ll, r, interp_transmittance_full, lambda_nm = plot_full_energy_component(channel_full, resolution_full, spectral_full)
+interp_transmittance_half, lambda_half = plot_half_energy_component()
 
 #Plotting code
 
@@ -333,6 +412,33 @@ def plot_contrast_contourplot(contrasts, delay_thickness, major_radius):
     cbar2 = plt.colorbar()
     plt.xlabel('Major Radius [m]')
     plt.ylabel('Delay plate thickness [mm]')
+    cbar2.set_label('Contrast', rotation=90)
+    plt.show()
+
+    return
+
+def plot_displacer_thickness(contrasts, displacer_thickness, major_radius):
+
+    pp,ll = np.meshgrid(displacer_thickness,major_radius)
+
+    levels=np.arange(0,0.43,0.05)
+
+    plt.figure()
+    plt.title('Delay plate = 15mm, Displacer Cut angle $\Theta$=45$^{\circ}$')
+    plt.pcolormesh(ll,pp/1000,contrasts,shading='gouraud')
+    cbar = plt.colorbar()
+    cbar.set_label('Contrast', rotation=90)
+    plt.xlabel('Major Radius [m]')
+    plt.ylabel('Displacer plate thickness [mm]')
+    plt.show()
+
+    plt.figure()
+    plt.title('Delay plate = 15mm, Displacer Cut angle $\Theta$=45$^{\circ}$')
+    CS =plt.contour(ll,pp/1000,contrasts, levels=levels)
+    plt.clabel(CS, inline=1, fontsize=10)
+    cbar2 = plt.colorbar()
+    plt.xlabel('Major Radius [m]')
+    plt.ylabel('Displacer plate thickness [mm]')
     cbar2.set_label('Contrast', rotation=90)
     plt.show()
 
