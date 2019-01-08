Fixed bugs in phase shift calculation in Crystal.py Added more information for general users to the phase delay calculation. Modified the Camera class so that the dimensions of the sensor can be arbitrary. Added a lens class to be used in the future for arbitrary lenses.

Model/Camera.py
@@ -1,23 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 from Tools.Plotting.graph_format import plot_format
 
 plot_format()
 
+"""
+Camera specifications for the Photron SA-4 and the PCO Edge cameras, two candidates for the imaging system. 
+Calculate the total signal to noise ratio including the read, dark and shot noise as measured for the two cameras.
+"""
+
 class Camera(object):
 
     def __init__(self, photron):
+
+        self.n_photons = 4.34*10**7
 
         if photron:
             self.photron_specs()
         else:
             self.pco_specs()
 
+        self.intensity = self.calculate_intensity()
+        self.shot_noise = self.calculate_shotnoise()
+        self.dark = self.calculate_darknoise()
+        self.SN = self.calculate_SN()
+
     def photron_specs(self):
-        self.n_pixels = 1024
+        self.name = 'Photron SA4'
+        self.n_pixels_x = 1024
+        self.n_pixels_y = 1024
         self.pixel_size = 20*10**-6 #m
-        self.sensor_size = 20.48*10**-3 #mm
+        self.sensor_size_x = 20.48*10**-3 #m
+        self.sensor_size_y = 20.48*10**-3 #m
         self.dark_noise = 3.55 #electrons
         self.dark_noise_error = 0.02
         self.read_out = 41.2
@@ -25,53 +39,50 @@ class Camera(object):
         self.fullwell_capacity = 45000
         self.quantum_efficiency = 0.4
         self.gain = 0.0862
+        self.integration_time = np.arange(1*10**-3, 20.5*10**-3, 0.5*10**-3)
 
     def pco_specs(self):
+        self.name = 'PCO Edge'
+        self.n_pixels_x = 2560
+        self.n_pixels_y = 2160
+        self.sensor_size_x = 16.64*10**-3
+        self.sensor_size_y = 14.04*10**-3
         self.dark_noise = 2 #electrons
         self.read_out = 2.2 #electrons
         self.fill_factor = 1 # how much of the pixel is actually available for light collection
         self.fullwell_capacity = 30000 #electrons
-        self.pixel_size = 6.5 #um
+        self.pixel_size = 6.5*10**-6
         self.gain = 0.46 #electrons/count
         self.quantum_efficiency = 0.54
+        self.integration_time = np.arange(5*10**-3, 20.5*10**-3, 0.5*10**-3)
 
+    def calculate_intensity(self):
+        self.intensity = self.n_photons * self.fill_factor * self.quantum_efficiency * self.integration_time * self.gain
+        return self.intensity
 
+    def calculate_shotnoise(self):
+        self.shot_noise = self.n_photons * self.integration_time * self.quantum_efficiency * self.gain**2
+        return self.shot_noise
 
-# Calculate signal to noise ratio as a function of integration time for the PCO edge camera vs the photron sa-4.
-#
-# def signal_to_noise(intensity, read, dark, shot):
-#     SN = intensity / np.sqrt(read**2 + dark + shot)
-#     return SN
-#
-# sa4 = Camera(photron=True)
-#
-# I_photons = 4.33*10**7 #photons/second - minimum emitted intensity output from msesim (average = 4.34*10^7) (min = 1.22E+06)
-# integration_time_sa4 = np.arange(1*10**-3, 20.5*10**-3, 0.5*10**-3)
-# integration_time_pco = np.arange(5*10**-3, 20.5*10**-3, 0.5*10**-3)
-#
-# intensity_sa4 = I_photons * sa4.fill_factor * sa4.quantum_efficiency * integration_time_sa4 * sa4.gain
-# shot_sa4 = I_photons * integration_time_sa4 * sa4.quantum_efficiency * sa4.gain**2
-# dark_sa4 = sa4.dark_noise * integration_time_sa4
-#
-# SN_sa4 = signal_to_noise(intensity_sa4, sa4.read_out, sa4.dark_noise, shot_sa4)
-#
-# print(SN_sa4)
-#
-# plt.figure()
-# plt.plot(integration_time_sa4*1000, SN_sa4, '--', label='sa4')
-# plt.xlim(0,10)
-# plt.xlabel('Integration time [ms]')
-# plt.ylabel('Signal to Noise')
-# plt.legend()
-# plt.show()
+    def calculate_darknoise(self):
+        self.dark = self.dark_noise * self.integration_time
+        return self.dark
 
+    def calculate_SN(self):
+        self.SN = self.intensity / np.sqrt(self.read_out ** 2 + self.dark + self.shot_noise)
+        return self.SN
 
+    def plot_SN(self):
 
+        plt.figure()
+        plt.plot(self.integration_time*1000, self.SN, '--', label=str(self.name))
+        plt.xlim(0,10)
+        plt.xlabel('Integration time [ms]')
+        plt.ylabel('Signal to Noise')
+        plt.legend()
+        plt.show()
 
-# pco = Camera(photron=False)
-# intensity_pco = I_photons * pco.fill_factor * pco.quantum_efficiency * integration_time_pco * pco.gain
-# shot_pco = I_photons * integration_time_pco * pco.quantum_efficiency * pco.gain**2
-# dark_pco = pco.dark_noise * integration_time_pco
-#
-# SN_pco = signal_to_noise(intensity_pco, pco.read_out, dark_pco, shot_pco)
+# Example
+# camera = Camera(photron=False)
+# camera.plot_SN()
 

Model/Crystal.py
@@ -3,108 +3,139 @@ import matplotlib.pyplot as plt
 
 from Model.Constants import Constants
 from Model.Camera import Camera
+from Model.Lens import Lens
 
 constants = Constants()
-camera = Camera(photron=True)
 
 class Crystal(object):
 
-    def __init__(self, wavelength, thickness, cut_angle, name, nx, ny, pixel_size, orientation, two_dimensional):
+    def __init__(self, camera, lens, thickness, cut_angle, orientation, wavelength, name):
 
         self.name = name
-        self.two_dimensional = two_dimensional
-        self.nx = nx
-        self.ny = ny
-        self.pixel_size = pixel_size
-        self.orientation = orientation * (np.pi/180.) #orientation of the optical axis (ie. vertical = 90 degrees)
-        self.wavelength = wavelength * 10 ** 6
-        self.cut_angle = cut_angle * (np.pi/180.) #optical axis in radians
-        self.L = thickness * 10**6
-        self.focal_length = 0.085 *10**6
-        self.angular_frequency = 2 * np.pi * (constants.c / self.wavelength) #in micrometers
+        self.thickness = thickness
+        self.cut_angle = cut_angle*(np.pi/180.)
+        self.orientation = orientation*(np.pi/180.)
+        self.wavelength = wavelength
 
-        x = np.linspace(-camera.sensor_size/2, camera.sensor_size/2, nx)
-        y = np.linspace(-camera.sensor_size/2, camera.sensor_size/2, ny)
-
-        self.xx, self.yy = np.meshgrid(x, y)
-
-        self.alpha, self.beta = self.non_axial_rays()
-        self.delta = self.beta - self.orientation
-
-
-        if self.name == 'lithium_niobate':
-            self.sc_ne = [2.9804, 0.02047, 0.5981, 0.0666, 8.9543, 416.08]
-            self.sc_no = [2.6734, 0.01764, 1.2290, 0.05914, 12.614, 474.60]
-
-            self.ne = self.refractive_index(sellmeier_coefficients=self.sc_ne, name=self.name)
-            self.no = self.refractive_index(sellmeier_coefficients=self.sc_no, name=self.name)
+        # if self.name == 'lithium_niobate':
+        #     self.sc_ne = [2.9804, 0.02047, 0.5981, 0.0666, 8.9543, 416.08]
+        #     self.sc_no = [2.6734, 0.01764, 1.2290, 0.05914, 12.614, 474.60]
+        #
+        #     self.ne = self.refractive_index(sellmeier_coefficients=self.sc_ne, name=self.name)
+        #     self.no = self.refractive_index(sellmeier_coefficients=self.sc_no, name=self.name)
 
         if self.name == 'alpha_bbo':
-            self.sc_ne = [2.3753, 0.012240, 0.016670, 0.01516]
-            self.sc_no = [2.7359, 0.018780, 0.018220, 0.01354]
+
+            self.sc_ne = [2.3753, 0.01224, 0.01667, 0.01516]
+            self.sc_no = [2.7359, 0.01878, 0.01822, 0.01354]
 
             self.ne = self.refractive_index(sellmeier_coefficients=self.sc_ne, name=self.name)
+
             self.no = self.refractive_index(sellmeier_coefficients=self.sc_no, name=self.name)
+
+            self.ne = np.sqrt(self.ne)
+            self.no = np.sqrt(self.no)
 
         self.birefringence = self.ne - self.no
 
-        if self.two_dimensional == True:
+        print(self.ne, self.no)
 
-            self.phi_total = self.phase_total()
+        x = np.linspace(-camera.sensor_size_x/2, camera.sensor_size_x/2, camera.n_pixels_x)
+        y = np.linspace(-camera.sensor_size_y/2, camera.sensor_size_y/2, camera.n_pixels_y)
 
-        else:
+        self.xx, self.yy = np.meshgrid(x,y)
 
-            self.phi_0 = self.phase_offset()
-            self.phi_shear = self.phase_shear()
-            self.phi_hyperbolic = self.phase_hyperbolic()
+        self.alpha, self.beta = self.include_non_axial_ray_angles(lens)
+        self.delta = self.beta - self.orientation
 
-    def refractive_index(self, sellmeier_coefficients, name):
+        self.phi = self.total_phase_delay()
 
-        if name == 'lithium_niobate':
-            return np.sqrt(1 + (sellmeier_coefficients[0]*self.wavelength**2)/(self.wavelength**2 - sellmeier_coefficients[1]) + (sellmeier_coefficients[2]*self.wavelength**2)/(self.wavelength**2 - sellmeier_coefficients[3]) + (sellmeier_coefficients[4]*self.wavelength**2) / (self.wavelength**2 - sellmeier_coefficients[5]))
+        self.phi_constant, self.phi_shear, self.phi_hyperbolic = self.phase_delay_to_second_order(lens)
 
-        if name == 'alpha_bbo':
-            return np.sqrt(sellmeier_coefficients[0] + (sellmeier_coefficients[1] / (self.wavelength** 2 - sellmeier_coefficients[2])) - (sellmeier_coefficients[3] * self.wavelength ** 2))
-        else:
-            print('Crystal not yet implemented!')
+    def include_non_axial_ray_angles(self, lens):
 
-    def non_axial_rays(self):
+        self.alpha = np.arctan(np.sqrt((self.xx)**2 + (self.yy)**2)/(lens.focal_length))
 
-        self.alpha = np.arctan(np.sqrt((self.xx)**2 + (self.yy)**2)/(self.focal_length*10**-6))
-        self.beta = np.arctan2(self.yy,self.xx)
+        self.beta = np.arctan2(self.yy,self.xx) + np.pi
 
         return self.alpha, self.beta
 
-    def frequency_dispersion(self):
+    def refractive_index(self, sellmeier_coefficients, name):
 
-        #Calculate derivatives using central differences method
+        if name == 'alpha_bbo':
+            return sellmeier_coefficients[0] + sellmeier_coefficients[1]/((self.wavelength*10**6)**2 - sellmeier_coefficients[2]) - (sellmeier_coefficients[3]*(self.wavelength*10**6)**2)
 
-        d_omega = np.gradient(self.angular_frequency)
-        d_birefringence = np.gradient(self.birefringence)
+        if name == 'lithium_niobate':
+            return 1 + (sellmeier_coefficients[0]*(self.wavelength*10**6)**2)/((self.wavelength*10**6)**2 - sellmeier_coefficients[1]) + (sellmeier_coefficients[2]*(self.wavelength*10**6)**2)/((self.wavelength*10**6)**2 - sellmeier_coefficients[3]) + (sellmeier_coefficients[4]*(self.wavelength*10**6)**2) / ((self.wavelength*10**6)**2 - sellmeier_coefficients[5])
+        else:
+            print('Crystal not yet implemented!')
 
-        dB_dw = d_birefringence/d_omega
+    def total_phase_delay(self):
 
-        self.kappa = 1 + (self.angular_frequency/self.birefringence)*(dB_dw)
+        """
+        Calculate the phase delay introduced by a uniaxial waveplate with an arbitrary optical axis orientation.
 
-        return self.kappa
+        Equation from "Phase shift formulas in uniaxial media: an application to waveplates", F. Veiras (2010). Orientation of the crystal is defined in this version of the equation as
+        90 degrees = parallel to the y axis of the crystal. Taking limits of this equation (derivation in A. Thorman's ANU thesis 2018)
 
-    #total phase shift between ordinary and extraordinary rays due to birefringent material
+        Required values:
 
-    def phase_total(self):
-        self.phi = ((2*np.pi*self.L*10**-6)/(self.wavelength*10**-6)) * ( (self.no**2 - constants.n_air**2*np.sin(self.alpha)**2)**0.5 + (constants.n_air*(self.no**2 - self.ne**2)*np.sin(self.cut_angle)*np.cos(self.cut_angle)*np.cos(self.delta)*np.sin(self.alpha))/(self.ne**2*np.sin(self.cut_angle)**2 + self.no**2*np.cos(self.cut_angle)**2) + ((-1*self.no * (self.ne**2 * (self.ne**2 * np.sin(self.cut_angle)**2 + (self.no**2 * np.cos(self.cut_angle)**2)) - (self.ne**2 - (self.ne**2 - self.no**2)*np.cos(self.cut_angle)**2 * np.sin(self.delta)**2)*constants.n_air**2*np.sin(self.alpha)**2)**0.5)/(self.ne**2*np.sin(self.cut_angle)**2 + self.no**2*np.cos(self.cut_angle)**2)))
+        wavelength: Wavelength of incident light in metres.
+
+        ne & no: Extraordinary and ordinary refractive indices for a given crystal and wavelength
+
+        beta: Azimuthal angle around the xy plane of the sensor (radians)
+
+        alpha: Incident angle onto the sensor (alpha = 0 is axial) (radians)
+
+        n_air: Refractive index of air
+
+        thickness: Thickness of the waveplate (m)
+
+        cut_angle: Orientation of the optical axis (radians)
+
+        :return: Delay in radians due to the waveplate. Same dimensions as the sensor size.
+        """
+
+        t1 = (self.no * np.sqrt(self.ne**2 * (self.ne**2 * np.sin(self.cut_angle)**2 + self.no**2 * np.cos(self.cut_angle)**2 ) - (self.ne**2 - (self.ne**2 - self.no**2)*np.cos(self.cut_angle)**2 * np.sin(self.beta - self.orientation)**2)*constants.n_air**2 * np.sin(self.alpha)**2)) / (self.ne**2 *np.sin(self.cut_angle)**2 + self.no**2 * np.cos(self.cut_angle)**2)
+
+        t2 = np.sqrt(self.no**2 - constants.n_air**2*np.sin(self.alpha)**2)
+
+        t3 = (constants.n_air * (self.no**2 - self.ne**2) * np.sin(self.cut_angle) * np.cos(self.cut_angle) * np.cos(self.beta + self.orientation) * np.sin(self.alpha))/(self.ne**2 * np.sin(self.cut_angle)**2 + self.no**2 * np.cos(self.cut_angle)**2)
+
+        self.phi = 2 * np.pi * (self.thickness/self.wavelength) * (t1 - t2 - t3)
+
         return self.phi
 
-    #individual terms in equation when taken to specific limits
+    def phase_delay_to_second_order(self, lens):
 
-    def phase_offset(self):
-        self.phi_0 = (-1 * 2 * np.pi * self.L * self.birefringence * np.cos(self.cut_angle) ** 2) / (self.wavelength)
-        return self.phi_0
+        """
 
-    def phase_shear(self):
-        self.phi_shear = (2 * np.pi * self.L * 10 ** -6 * self.birefringence * np.sin(2 * self.cut_angle)) / (2 * self.wavelength * 10 ** -6 * ((self.ne + self.no) / 2) * self.focal_length * 10 ** -6) #*self.yy
-        return self.phi_shear
+        :param lens: Class containing information on the focussing lens used between the crystal and camera sensor (focal length in metres is required)
+        :return: phi_constant: Constant phase delay offset due to zeroth order term in Veiras' formula (Radians)
+                 phi_shear: Linear phase delay from first order term (radians)
+                 phi_hyperbolic: Quadratic phase delay from second order term (radians)
+        """
 
-    def phase_hyperbolic(self):
-        self.phi_hyperbolic = (-2 * np.pi * self.L*10**-6 * self.birefringence / (4 * ((self.ne + self.no)/2)**2 * self.wavelength*10**-6) * (self.focal_length * 10**-6) ** 2)
-        return self.phi_hyperbolic
+        # individual terms in equation when taken to specific limits - For more info read Alex Thorman's ANU thesis (2018)
 
+        self.phi_constant = ((2 * np.pi * self.thickness * self.birefringence * np.cos(self.cut_angle) ** 2) / (self.wavelength)) * np.ones((np.shape(self.xx)))
+
+        self.phi_shear = ( (-2 * np.pi / self.wavelength) * self.thickness * self.birefringence * np.sin(2*self.cut_angle) * self.yy) / ( ((self.ne + self.no)/2) * lens.focal_length )
+
+        self.phi_hyperbolic = ((2 * np.pi / self.wavelength) * self.thickness * self.birefringence) / (4 * ((self.ne + self.no)/2)**2 * (lens.focal_length)**2) * ((3-np.cos(2*self.cut_angle))*(self.xx**2) - (3*np.cos(2*self.cut_angle)-1)*(self.yy**2))
+
+        return self.phi_constant, self.phi_shear, self.phi_hyperbolic
+
+
+camera = Camera(photron=False)
+lens = Lens(name='collection lens', focal_length=85*10**-3, diameter=None, aperture=None, f=None)
+crystal = Crystal(camera, lens, thickness=1*10**-3, cut_angle=45, orientation=90, wavelength=660*10**-9, name='alpha_bbo')
+
+levels = np.arange(-50,60,10)
+
+plt.figure()
+cs = plt.contour(crystal.xx, crystal.yy, crystal.phi_shear, levels=levels)
+plt.clabel(cs, inline=1, fontsize=10)
+plt.colorbar()
+plt.show()