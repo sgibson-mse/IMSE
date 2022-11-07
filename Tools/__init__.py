3ea00be0d66d8268baabb31da08631a5701c4eb5
Gibson, Sam <sam.gibson@ukaea.uk>
2022-11-07 17:43:57 +0000

    Repo tidy up

--- Tools/__init__.py
+++
@@ -1,1 +0,0 @@
-from IMSE.Tools import load_msesim
--- Tools/micropolariser_mse.py
+++
@@ -1,4 +0,0 @@
-"""
-Coding up the micropolariser IMSE system to see whether we can retrieve the polarisation information in any of the terms.
-"""
-
